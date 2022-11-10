f900a2afdfa3d45efe175171d4679122c138019f
nejmiyassine <devtwins0@gmail.com>
2022-11-10 12:38:58 +0100

    If Route doesn't exist redirect to home page

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 // Pages
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
@@ -23,6 +23,10 @@ export const router = createBrowserRouter([
       {
         path: '/post/:postId',
         element: <Single />,
+      },
+      {
+        path: '/*',
+        element: <Navigate to='/' />,
       },
     ],
   },
